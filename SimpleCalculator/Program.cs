b58f286ec6e73763d7078c00737e614965dbf2fd
NgyuenDakage <shawn.nabizada@gmail.com>
2024-09-11 18:23:44 -0400

    Added loop for user to enter a valid input.

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,28 +6,48 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            try
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+            double firstNumber = 0;
+            Boolean checkInput = true;
+            while (checkInput)
             {
-                // Class to convert user input
-                InputConverter inputConverter = new InputConverter();
-
-                // Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
-
-                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
-                double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
-                string operation = Console.ReadLine();
-
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
-
-                Console.WriteLine("The value " + firstNumber + " plus the value " + secondNumber + " is equal to " + result);
-
-            } catch (Exception ex)
-            {
-                // Normally, we'd log this error to a file.
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("Input first number: ");
+                String input1 = Console.ReadLine();
+                if (double.TryParse(input1, out double r))
+                {
+                    firstNumber = InputConverter.ConvertInputToNumeric(input1);
+                    checkInput = false;
+                }
+                else
+                {
+                    System.Console.WriteLine("Conversion error: please enter a valid number");
+                }
             }
 
+            double secondNumber = 0;
+            Boolean checkSecondInput = true;
+            while (checkSecondInput)
+            {
+                Console.WriteLine("Input second number: ");
+                String input2 = Console.ReadLine();
+                if (double.TryParse(input2, out double r))
+                {
+                    secondNumber = InputConverter.ConvertInputToNumeric(input2);
+                    checkInput = false;
+                }
+                else
+                {
+                    System.Console.WriteLine("Conversion error: please enter a valid number");
+                }
+            }
+
+
+            string operation = Console.ReadLine();
+
+            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+            Console.WriteLine("The value " + firstNumber + " plus the value " + secondNumber + " is equal to " + result);
         }
     }
 }
