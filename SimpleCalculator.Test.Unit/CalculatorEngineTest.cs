fe88be07b799166cc9838960a748964d61471b84
REtry42 <149620675+REtry42@users.noreply.github.com>
2024-09-15 20:34:34 -0400

    Added test cases

--- SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
+++ SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -31,5 +31,93 @@ namespace SimpleCalculator.Test.Unit
             Assert.AreEqual(3, result);
 
         }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = tool.Calculate("subtract", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void SubtractsTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = tool.Calculate("-", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 2;
+            double result = tool.Calculate("multiply", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void MultipliesTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 1;
+            int number2 = 2;
+            double result = tool.Calculate("*", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = tool.Calculate("divide", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void DividesTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 4;
+            int number2 = 2;
+            double result = tool.Calculate("/", number1, number2);
+            Assert.AreEqual(2, result);
+
+        }
+
+        [TestMethod]
+        public void ModulusTwoNumbersAndReturnsValidResultForNonSymbolOpertion()
+        {
+
+            int number1 = 5;
+            int number2 = 2;
+            double result = tool.Calculate("modulus", number1, number2);
+            Assert.AreEqual(1, result);
+
+        }
+
+        [TestMethod]
+        public void ModulusTwoNumbersAndReturnsValidResultForSymbolOpertion()
+        {
+
+            int number1 = 5;
+            int number2 = 2;
+            double result = tool.Calculate("%", number1, number2);
+            Assert.AreEqual(1, result);
+
+        }
     }
 }
